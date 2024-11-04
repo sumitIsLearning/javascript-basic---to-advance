491e97cf720313c8fe5d028141301623184aa701
sumitIsLearning <try.sumitchakraborty@gmail.com>
2024-11-04 15:32:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+// In the previous lesson we talked about varialble and how to define them and i have given you some tips
+// In this lesson we will talk about different datatypes
+// before that let talk about some important stuff of js
+"use strict"; // when you say "use strict" , the interpreter starts treating all you code as newer version of js
+
+// Now lets move to the actuall part of the lesson and talk about datatypes
+// The datatypes are divided into two parts primitive datatype(system defined) and non-primitive datatypes
+/*  7 primitive datatypes
+        Number
+        string
+        Bigint
+        Boolean
+        Null
+        Undefined
+        Symbol
+*/
+
+/* non-primitive datatype
+        Object
+*/
+
+
+// Now let try them one by one
