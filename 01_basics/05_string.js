3227329bbe8d29e59347c2de4008873b397d39fb
sumitIsLearning <try.sumitchakraborty@gmail.com>
2024-11-22 10:10:12 +0000

    Number and Math

---
+++ 01_basics/05_string.js
@@ -0,0 +1,10 @@
+// simple formula to get random number between min and max values
+
+const min = 10;
+const max = 20;
+//Math.floor((Math.random() * (max - min + 1)) + min) // => formula
+for(let i = 0; i < 20; i++) {
+    
+    console.log(Math.floor((Math.random() * (max - min + 1)) + min));
+    
+}
