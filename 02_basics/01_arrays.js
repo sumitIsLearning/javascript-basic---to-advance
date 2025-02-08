7e111b3811de11ceebf77f5f68f842b968a0aaf0
sumitIsLearning <try.sumitchakraborty@gmail.com>
2025-02-08 05:09:29 +0000

    revised and learned about new methods

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -35,8 +35,15 @@ console.log(arr);
 console.log(arr.splice(0 , 1 , 25)) //=> Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
 console.log(arr);
 
-
 console.log(arr.join());
+
+console.log(arr.reduce((prev , curr) => {
+    return prev + curr;
+})); // **Note: prev starts with index 0 item and curr starts with index 1 item , and as calculation goes on the prev value is replaced with the calculated value.
+console.log(arr.reduce((prev , curr) => {
+    return prev + curr;
+},5)); // **Note: Now prev will start with the value given(5) and curr with start with index 0 item
+
 
 // Array's data type is an object
 // Array can store different datatypes
@@ -51,11 +58,12 @@ console.log(arr.join());
  * @unshift -> adds one or more element at the front of the array and returns the new lenght of the array
  * @shift -> removes the first of an array and returns the element
  * @slice -> Extract a section of the calling array and returns a new array , without modifying the original array
- * @splice -> adds and/or removes element from the calling array
+ * @splice -> adds and/or removes element from the calling array,effects original array.Returns the deleted elements
  * @join -> joins all element of an array with the given seperator into a string
  * @map -> returns a new array containing the result of invoking a function on every element of the calling array
  * @findIndex -> Returns the index of the first element in the array that satisfies the provided testing function, or -1 if no appropriate element was found.
  * @find -> Returns the value of the first element in the array that satisfies the provided testing function, or -1 if no appropriate element was found.
  * @includes -> Determines whether the calling array contains a value, returning true or false as appropriate
  * @toSorted -> Returns a new array with the elements sorted in ascending order, without modifying the original array.
+ * @reduce --> Returns a single value based on the reducer(callback function), the calculati on happens , where two arguments are present in as previous , current in the call back function , where previous value starts with index 0 item , if no initial value is provided , and current starts with index 1 item and as the calculation progresses the previous value is replaced with the calculated value and curr value progreess as index 1 , 2 ...n.if no items / empty array is provided it throws and error.
  */
